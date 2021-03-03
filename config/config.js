aa8489904a23c5fa2eb5672bfd8268773fb755bd
Bruce Wiseman <bruce.wiseman@hotmail.com>
2021-03-03 22:05:10 +0000

    Added mongoDB uri to enviroment variables, working connection to cluster

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
  env: process.env.NODE_ENV || 'development',
  port: process.env.PORT || 3000,
  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
- mongoUri: process.env.MONGODB_URI ||
+ mongoUri: process.env.MONGODB_URI || 'mongodb+srv://bruce_wiseman:BruceW1seman@cluster0.imavv.mongodb.net/serverDB?retryWrites=true&w=majority' ||
  process.env.MONGO_HOST ||
  'mongodb://' + (process.env.IP || 'localhost') + ':' +
  (process.env.MONGO_PORT || '27017') +
