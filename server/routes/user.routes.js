daacf8a8187248e16667111bca494f790864b05c
Bruce Wiseman <bruce.wiseman@hotmail.com>
2021-03-04 01:55:14 +0000

    Last stuff tested, user stuff added working, user auth working, tested.

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import userCtrl from './user.controller'
+import authCtrl from './auth.controller'
+
 const router = express.Router()
 router.route('/api/users')
  .get(userCtrl.list)
  .post(userCtrl.create)
 router.route('/api/users/:userId')
- .get(userCtrl.read)
- .put(userCtrl.update)
- .delete(userCtrl.remove)
+ .get(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.read)
+ .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+ .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
+
 router.param('userId', userCtrl.userByID)
 export default router
