f520041ee2b6c9c5bbac83a0ed1c5ed979c2acb0
Bruce Wiseman <bruce.wiseman@hotmail.com>
2021-03-04 16:50:23 +0000

    Added React to config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# A_B_Metric
+# Web Enterprise Systems Coursework
 CM4025 Web Enterprise Systems Coursework Part 2
+Bruce Genari-Wiseman, 1606404
--- server/express.js
+++ server/express.js
@@ -9,8 +9,14 @@ import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
+//comment out before building for production
+import devBundle from './devBundle'
 
 const app = express()
+const CURRENT_WORKING_DIR = process.cwd()
+
+//comment out before building for production
+devBundle.compile(app)
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -21,8 +27,10 @@ app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use('/', userRoutes)
 app.use('/', authRoutes)
+
 
 app.use((err, req, res, next) => {
  if (err.name === 'UnauthorizedError') {
